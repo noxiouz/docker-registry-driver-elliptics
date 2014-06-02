7ad6a63e9ec24e5f77c80ad7fc73ce317f5a5442
Anton Tyurin <noxiouz@yandex.ru>
2014-06-02 18:15:44 +0400

    first step

--- tests/test.py
+++ tests/test.py
@@ -4,6 +4,8 @@ import logging
 
 from docker_registry import testing
 
+from nose import tools
+
 logger = logging.getLogger(__name__)
 
 
@@ -17,3 +19,10 @@ class TestDriver(testing.Driver):
         self.scheme = 'elliptics'
         self.path = ''
         self.config = testing.Config({"elliptics_nodes": "localhost:1025:2-0"})
+
+
+@tools.raises(ValueError)
+def test_elliptics_nodes_conf():
+    config = testing.Config({})
+    driver = testing.Driver(config=config)
+    driver.setUp()
