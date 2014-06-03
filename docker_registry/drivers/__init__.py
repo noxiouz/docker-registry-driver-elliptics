f901babfc536a4666365a7f495fb0a1842d1dcf3
Anton Tyurin <noxiouz@yandex.ru>
2014-06-03 19:45:14 +0400

    Exclude __init__.py from coverage

--- docker_registry/drivers/__init__.py
+++ docker_registry/drivers/__init__.py
@@ -1,7 +1,7 @@
 # Namespace dance baby
-try:
+try:  # pragma: no cover
     import pkg_resources
     pkg_resources.declare_namespace(__name__)
-except ImportError:
+except ImportError:  # pragma: no cover
     import pkgutil
     __path__ = pkgutil.extend_path(__path__, __name__)
